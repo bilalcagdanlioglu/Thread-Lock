df3acad417c953618c19974f0a1f89e6cd02dbd5
Bilal Cağdanlıoğlu <69059651+bilalcagdanlioglu@users.noreply.github.com>
2022-01-21 15:02:36 +0300

    First commit

---
+++ ListWorker.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListWorker {
+
+	Random random = new Random();
+	ArrayList<Integer> list1 = new ArrayList<Integer>();
+	ArrayList<Integer> list2 = new ArrayList<Integer>();
+	
+	private Object lock1 = new Object();
+	private Object lock2 = new Object();
+	
+	public void list1DegerEkle() {
+		
+		synchronized(lock1) {
+		
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		list1.add(random.nextInt(100));
+		}
+	}
+	
+	public void list2DegerEkle() {
+		
+		synchronized(lock2) {
+		
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		list2.add(random.nextInt(100));
+		}
+	}
+	
+	public void degerAta() {
+		for(int i =0;i<1000;i++) {
+			list1DegerEkle();
+			list2DegerEkle();
+		}
+	}
+	
+	public void calistir() {
+		Thread thread1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				degerAta();
+				
+			}
+		});
+		
+		Thread thread2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				degerAta();
+				
+			}
+		});
+		long baslangic = System.currentTimeMillis(); 
+		
+		thread1.start();
+		thread2.start();
+		
+		try {
+			thread1.join();
+			thread2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("List1 size: "+list1.size() +" List2 size: "+list2.size());
+		long bitis = System.currentTimeMillis();
+		System.out.println("Süre: "+ (bitis - baslangic));
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		ListWorker worker = new  ListWorker();
+		worker.calistir();
+	}
+
+}
